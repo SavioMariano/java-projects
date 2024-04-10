b03b4ccb3197de97ddd6282c369f5ed199c10977
Savio <78896606+SavioMariano@users.noreply.github.com>
2024-04-10 16:21:47 -0300

    Update Teste.java

--- HERANÇA-JAVA/DADOS/Teste.java
+++ HERANÇA-JAVA/DADOS/Teste.java
@@ -3,16 +3,17 @@ public class Teste{
     public static void main(String[] args) {
         Pessoa Pai = new Pessoa("Júlio", 1997);
         Aluno Filho = new Aluno("Mateus", 2008,20978, "Informática");
-        Professor Humberto = new Professor("Humberto", 1975 , null, 0, 8);
-        Funcionario  Zé = new Funcionario("Zé", 1993,"Zelador" ,1400.15);
+        Professor Humberto = new Professor("Humberto", 1975 , null, 45790, 8);
+        Funcionario  Zé = new Funcionario("Zé", 1993,"Zelador" ,1400.15, 28457);
 
     System.out.println("\nDados do pai: " + Pai.Saudação());
 
-    System.out.println("\nDados do filho: " + Filho.SaudaçãoAluno());
+    System.out.println("\nDados do filho: " + Filho.Saudação());
 
-    System.out.println("\nDados do professor: " + Humberto.SaudaçãoProfessor());
+    System.out.println("\nDados do professor: " + Humberto.Saudação());
 
-    System.out.println("\nDados do funcionário: " + Zé.SaudaçãoFunc() + "\n");
+    System.out.println("\nDados do funcionário: " + Zé.Saudação() + "\n");
+
 
     System.out.println("Saudações alteradas:");
 
@@ -20,7 +21,7 @@ public class Teste{
     System.out.println("\nDados do professor: " + Humberto.Saudação());
 
         //Usando o metódo Saudação do aluno
-    System.out.println("\nDados do professor: " + Humberto.SaudaçãoAluno());
+    System.out.println("\nDados do professor: " + Humberto.Saudação());
 
         //Usando o metódo Saudação da Pessoa
     System.out.println("\nDados do funcionário: " + Zé.Saudação() + "\n");
