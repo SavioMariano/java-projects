4c87eb0dc60147596eb0c13e40052a75ea60b02d
Savio <78896606+SavioMariano@users.noreply.github.com>
2024-04-10 16:21:21 -0300

    Update Professor.java

--- HERANÇA-JAVA/src/Herança/Professor.java
+++ HERANÇA-JAVA/src/Herança/Professor.java
@@ -1,6 +1,4 @@
 package Herança;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 
 public class Professor extends Aluno{
     private int Formação;
@@ -8,21 +6,9 @@ public class Professor extends Aluno{
     super(Name,Birth,Matricula,Curso);   
     this.Formação = Formação;
     }
-    public String SaudaçãoProfessor(){
-       LocalTime NowHour = LocalTime.now();
-       String FormatHour = NowHour.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-       int hour = NowHour.getHour();
-   
-       if (hour >= 5 && hour <= 11) {
-          return FormatHour + " Bom dia " + Name + " você tem " + CalcularIdade() + " anos e " + Formação + " anos de Formação.";
-       }else if (hour >= 12 && hour <= 18) {
-           return FormatHour + "Bom tarde " + Name + " você tem " + CalcularIdade() + " anos e " + Formação + " anos de Formação.";
-       }else if (hour >= 19 && hour <= 23) {
-           return FormatHour + "Boa noite " + Name + " você tem " + CalcularIdade() + " anos e " + Formação + " anos de Formação.";
-       }else if (hour >= 24 && hour <= 4){
-           return FormatHour + "Boa madrugada " + Name + " você tem " + CalcularIdade() + " anos e " + Formação + " anos de Formação.";
-       }else{
-           return "Não achamos seus dados...";
-   }
+    @Override
+    public String Saudação(){
+    String Saudation = super.Saudação();
+    return Saudation + " tenho " + this.Formação + " anos";
  }
 }
